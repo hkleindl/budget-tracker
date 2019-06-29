400c5de0ea015b53bfd83a36c6e45cfa664fd3e1
hkleindl <hkleindl@gmail.com>
2019-06-28 22:29:08 -0400

    Add Transaction attributes

--- db/migrate/20190629021501_create_transactions.rb
+++ db/migrate/20190629021501_create_transactions.rb
@@ -1,7 +1,11 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-
+      t.decimal :amount
+      t.string :type
+      t.text :description
+      t.integer :category_id
+      t.integer :budget_id
       t.timestamps
     end
   end
