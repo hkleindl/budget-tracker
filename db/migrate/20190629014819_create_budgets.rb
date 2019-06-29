67a5df66ea80fe9df0f5a06024135a2e881ca0ac
hkleindl <hkleindl@gmail.com>
2019-06-28 22:13:26 -0400

    Add attributes to Budget

--- db/migrate/20190629014819_create_budgets.rb
+++ db/migrate/20190629014819_create_budgets.rb
@@ -1,7 +1,11 @@
 class CreateBudgets < ActiveRecord::Migration[5.2]
   def change
     create_table :budgets do |t|
-
+      t.string :name
+      t.string :type
+      t.decimal :starting_balance
+      t.decimal :current_balance
+      t.integer :user_id
       t.timestamps
     end
   end
