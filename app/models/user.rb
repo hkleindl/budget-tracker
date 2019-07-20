859850aa1335e029384455d7047a8ff3bf994d22
hkleindl <hkleindl@gmail.com>
2019-07-20 14:49:46 -0400

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
   has_many :budgets
   has_many :transactions, through: :budgets
   has_many :categories
+
+  has_secure_password
+
+  validates :username, presence: true,
+  uniqueness: true
+
+  validates :email, 
+  presence: true,
+  uniqueness: true
+
+  
 end
